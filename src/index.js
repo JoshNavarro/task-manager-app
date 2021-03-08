5905331704264f94e67cce2e2eb94d49fcce7a15
Joshua Navarro <jnavarro4598@gmail.com>
2021-03-07 23:36:08 -0800

    Added authentication and user capabilities

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Service is in maintenance mode')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -13,3 +25,18 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+    // const task = await Task.findById('6045a9d87f4c046d8bb36c12')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
+
+    const user = await User.findById('6045a90a2b91136a1487b93c')
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks)
+}
+
+main()
